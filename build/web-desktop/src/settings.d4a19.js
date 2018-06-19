88ca78b5742c6d9c5dfb5305968bf0af667f413e
buhandong <buhandong@163.com>
2018-06-19 11:33:23 +0800

    add build files

---
+++ build/web-desktop/src/settings.d4a19.js
@@ -0,0 +1,161 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "bcpDX4eD1EsbukLcualSca": [
+                "res/textures/bg.jpg",
+                0
+            ],
+            "66UgaVM0hHJYty9Fueh0fm": [
+                "res/textures/reversi/Black.png",
+                0
+            ],
+            "2aoC1kjWdAkLnjjXtxy3pr": [
+                "res/textures/reversi/White.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ],
+            "d608qFRoFHwbXd0Dap056i": [
+                "image/default_scrollbar_vertical.png",
+                0
+            ],
+            "61cyPdEfRN047sDK9rO0W5": [
+                "image/default_scrollbar_vertical_bg.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ],
+            "b2aHrECZ5APKGS/0d2hvT1": [
+                "particle/atom.plist",
+                1
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.ParticleAsset"
+    ],
+    jsList: [
+        "assets/src/reversi_online/socket.io.56128.js"
+    ],
+    launchScene: "db://assets/res/scenes/loding.fire",
+    scenes: [
+        {
+            url: "db://assets/res/scenes/loding.fire",
+            uuid: 7
+        },
+        {
+            url: "db://assets/res/scenes/hall.fire",
+            uuid: 3
+        },
+        {
+            url: "db://assets/res/scenes/match.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/res/scenes/reversi_online.fire",
+            uuid: 1
+        },
+        {
+            url: "db://assets/res/scenes/reversi.fire",
+            uuid: 9
+        }
+    ],
+    packedAssets: {
+        "05ed2f": [
+            0,
+            2,
+            5,
+            6
+        ],
+        "0aa5c72ff": [
+            1,
+            "6cHm7ZcyZJb6up5Kb1ZO/6",
+            "8bxvtQNbxLEpZUi1HsPmMU",
+            4,
+            8,
+            "fcHtry67JKe4VQUsGtU6n0"
+        ],
+        "0b1bf8e52": [
+            2,
+            "5cO7kybDxGj4ipyMYdRYZB",
+            "5f5dyqtRNNxaFmVzYns6FZ",
+            3,
+            4,
+            "9bvaMerUlDyary99mJa6xp",
+            5,
+            6
+        ],
+        "0b7146e97": [
+            7,
+            "4fhQ8WQRxAr4BjnTF7Byv1",
+            4,
+            8
+        ],
+        "0c49ae998": [
+            9,
+            4,
+            8
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "15UZwpCk5Ng5krsvg74Wc0",
+        "1cRhKiLU5IwYJJsr2DrQa0",
+        "29FYIk+N1GYaeWH/q1NxQO",
+        "8acfqbljRHXK2shXdwXQRJ",
+        "94nZeiRMRHOI4JOOrCtcLD",
+        "e97GVMl6JHh5Ml5qEDdSGa",
+        "f0BIwQ8D5Ml7nTNQbh1YlS",
+        "0dnG4Oh6tGRbZ3rrL5WUw/",
+        "a2MjXRFdtLlYQ5ouAFv/+R",
+        "06Xx+0McJFHpmNoJBf16Cv"
+    ],
+    "md5AssetsMap": {
+        "05/05ed2f.json": "a7cba",
+        "0a/0aa5c72ff.json": "6f630",
+        "0b/0b1bf8e52.json": "ed720",
+        "0b/0b7146e97.json": "fbe57",
+        "0c/0c49ae998.json": "35f13",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f.json": "14a51",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f/zhankukuaile.ttf": "0acea",
+        "assets/res/textures/bg.jpg": "e7b3b",
+        "assets/res/textures/reversi/Black.png": "157b8",
+        "assets/res/textures/reversi/White.png": "46b19",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_panel.png": "cdbc9",
+        "internal/image/default_scrollbar_vertical_bg.png": "4bb41",
+        "internal/image/default_scrollbar_vertical.png": "71821",
+        "internal/image/default_sprite_splash.png": "cea68",
+        "internal/particle/atom.plist": "e31bf"
+    }
+};
